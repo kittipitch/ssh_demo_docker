c557c7ae2afe289354a08134c893e8b28bea105a
Kittipitch Kuptavanich <kittipitch@users.noreply.github.com>
2025-08-08 00:47:06 +0700

    Update build_docker.sh
    use docker compose

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Bringing up container using docker-compose..."
-docker-compose up -d --build
+docker compose up -d --build
 
 echo "Container started with fixed IP 172.20.0.100"
 
